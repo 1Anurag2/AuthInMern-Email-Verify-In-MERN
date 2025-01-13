f1330564354874ff9a11f3653238c4d8f4713086
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-01-13 23:54:43 +0530

    changes completed

--- AuthInMern-Email-Verify-In-MERN/server/utils/sendEmail.js
+++ AuthInMern-Email-Verify-In-MERN/server/utils/sendEmail.js
@@ -1,22 +1,6 @@
 const nodemailer = require("nodemailer");
 
 module.exports = async (email, subject) => {
-	const text = `
-Subject: Excited to Join the Google Team in the Account Sector!
-
-Dear Dimpal Verma,
-
-I am thrilled to share the news that I have been offered an opportunity to join Google in the Account Sector! I would like to express my sincere gratitude to you and the entire hiring team for believing in my skills and offering me this exciting role.
-
-I am looking forward to contributing to the team, learning from the best in the industry, and making an impactful contribution to Google’s success. I am eager to start this new journey and collaborate with such a talented group of professionals.
-
-Thank you once again for this wonderful opportunity, and I am excited to begin this new chapter with Google.
-
-Best regards,
-Name : Dimpal Verma
-Contact : 8953340290
-
-`;
 	try {
 		const transporter = nodemailer.createTransport({
 			host: process.env.HOST,
@@ -30,9 +14,9 @@ Contact : 8953340290
 		});
 
 		await transporter.sendMail({
-			from: "Google",
+			from: user,
 			to: email,
-			subject: " Application for Accountant Position",
+			subject: subject,
 			text: text,
 		});
 		console.log("email sent successfully");
